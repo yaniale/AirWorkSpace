4393d66ef0437fb4a20e78b1c6f9a398cc958bd2
Ruben Carballo <rcarballoq@gmail.com>
2022-04-12 22:08:58 +0200

    CENTER ROUTER
    Minor Adjustment

--- api/routes/center.router.js
+++ api/routes/center.router.js
@@ -21,14 +21,14 @@ const {
 } = require('../controllers/booking.controller')
 
 router.get('/', getAllCenters)
-router.post('/', checkAuth, checkHost, createCenter)
 router.get('/:id', getCenter)
+router.post('/', checkAuth, checkHost, createCenter)
+router.post('/:id/bookings', checkAuth, createBooking)
 router.put('/:id/allotment', checkAuth, checkHost, manageAllotment)
 router.put('/:id/rateplan', checkAuth, checkHost, manageRatePlan)
-router.post('/:id/bookings', checkAuth, createBooking)
+router.put('/:id/bookings/:bookingId', checkAuth, checkHost, updateBookingStatus)
 router.put('/:id', checkAuth, checkHost, updateCenter)
 router.delete('/:id', checkAuth, checkHost, deleteCenter)
-router.put('/:id/bookings/:bookingId', checkAuth, checkHost, updateBookingStatus)
 
 
 module.exports = router
